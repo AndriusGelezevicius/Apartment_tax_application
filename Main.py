1bb647b40d2f3ef91a00bda056dadbcf0ebaa086
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-11-02 10:56:25 +0200

    add buttons to main window

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font, ttk
 
 
 
@@ -7,9 +8,16 @@ root = tk.Tk()
 root.title("Apartment tax application")
 root.geometry("300x500")
 
+custom_font = font.Font(size=12, weight="bold")
+label = tk.Label(root, text="Apartment tax application", font=custom_font)
+label.pack(pady=30)
 
-label = tk.Label(root, text="Apartment tax application")
-label.pack(pady=20)
+new_tax_button = ttk.Button(root, text="New tax", command="new_tax")
+new_tax_button.pack(pady=20)
+
+history_tax_button = ttk.Button(root, text="Previous taxes", command="history_tax")
+history_tax_button.pack(pady=20)
+
 
 
 
