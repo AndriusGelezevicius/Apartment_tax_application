0742096b265d896051251448a33337fe9a991b3c
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-11-02 10:31:03 +0200

    add Label on main window

---
+++ Main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+
+# Main window
+root = tk.Tk()
+root.title("Apartment tax application")
+root.geometry("300x500")
+
+
+label = tk.Label(root, text="Apartment tax application")
+label.pack(pady=20)
+
+
+
+root.mainloop()
