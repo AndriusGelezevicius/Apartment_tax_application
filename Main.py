cbd9361395a7e528e08b44f088aa844087ee2304
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-11-02 11:17:01 +0200

    add developer label and a image

--- Main.py
+++ Main.py
@@ -8,16 +8,24 @@ root = tk.Tk()
 root.title("Apartment tax application")
 root.geometry("300x500")
 
+image = tk.PhotoImage(file="C:/Users/PC/Desktop/Python/pythonProject/Apartment tax application/Images/taxes.png")
+
+image_label = tk.Label(root, image=image)
+image_label.pack(pady=30)
+
 custom_font = font.Font(size=12, weight="bold")
 label = tk.Label(root, text="Apartment tax application", font=custom_font)
-label.pack(pady=30)
+label.pack(pady=10)
 
-new_tax_button = ttk.Button(root, text="New tax", command="new_tax")
+button_width = 20
+new_tax_button = ttk.Button(root, text="New tax", command="new_tax", width=button_width)
 new_tax_button.pack(pady=20)
 
-history_tax_button = ttk.Button(root, text="Previous taxes", command="history_tax")
+history_tax_button = ttk.Button(root, text="Previous taxes", command="history_tax", width=button_width)
 history_tax_button.pack(pady=20)
 
+developer_label = tk.Label(root, text="Developed by Andrius Geleževičius")
+developer_label.pack(side="bottom", pady=10, anchor="s")
 
 
 
